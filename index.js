8c62a6c558bbc96d26c8d0043b2c41a6dcaaac09
“VIOLET-BOSIRE” <violet.bosire@student.moringaschool.com>
2022-11-30 14:05:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names){
+    let giftCards = []
+for(let i=0;i<names .length;i++){
+    giftCards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+
+}
+return giftCards
+
+}
+function countDown(number){
+    while(number>=0){
+        console.log(number--);
+    }
+}
